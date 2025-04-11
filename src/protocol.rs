3af829625bbb9ded45f5248516687f91f788ea31
gagem-sigsev <erebus.sigsev@gmail.com>
2025-04-11 16:40:14 -0500

    Implement send_packet functionality

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,6 +1,6 @@
 use crate::packet::Packet;
-use std::net;
-
+use std::io::Write;
+use std::net::{Ipv4Addr, SocketAddrV4, TcpListener};
 #[derive(Debug)]
 pub struct Erebus {
     packets_sent: u32,
@@ -30,5 +30,13 @@ impl Erebus {
         );
     }
 
-    pub fn send_packet(&self, spkt: &Packet) {}
+    pub fn send_packet(&self, spkt: &Packet) -> std::io::Result<()> {
+        let addr = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8080);
+        let listener = TcpListener::bind(addr)?;
+        let (mut stream, clientaddr) = listener.accept().expect("Unable to accept client");
+        println!("Client: {:?} has connected...", clientaddr);
+        let spkt = spkt.serialize_packet();
+        stream.write_all(&spkt[0..spkt.len()])?;
+        Ok(())
+    }
 }
