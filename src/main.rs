fa5af9a4d4add1913d107e92b2b81ec4d6c550e7
gagem-sigsev <erebus.sigsev@gmail.com>
2025-04-26 01:20:55 -0500

    Modify send_packet impl method to send to specified dst ip

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ use crate::protocol::Erebus;
 fn main() {
     let src = String::from("127.0.0.1");
     let dst = String::from("127.0.0.1");
-    let payload = String::from("Erebus packet!");
+    let payload = String::from("Erebus packet!\n");
     let checksum = Packet::calculate_checksum(&payload);
     let data_length = payload.len() as u32;
     let packet: Packet = Packet::new(src, dst, payload, data_length, checksum);
 
-    let erebus = Erebus::new();
+    let mut erebus = Erebus::new();
     erebus.log_protocol();
     erebus.log_packet(&packet);
 
--- src/protocol.rs
+++ src/protocol.rs
@@ -1,10 +1,11 @@
 use crate::packet::Packet;
 use std::io::Write;
 use std::net::{Ipv4Addr, SocketAddrV4, TcpListener};
+use std::str::FromStr;
 
 #[derive(Debug)]
 pub struct Erebus {
-    packets_sent: u32,
+    pub packets_sent: u32,
     packets_recieved: u32,
     packets_total: u32,
 }
@@ -32,13 +33,17 @@ impl Erebus {
     }
 
     // TODO: implement custom transport layer
-    pub fn send_packet(&self, spkt: &Packet) -> std::io::Result<()> {
-        let addr = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8080);
-        let listener = TcpListener::bind(addr)?;
-        let (mut stream, clientaddr) = listener.accept().expect("Unable to accept client");
-        println!("Client: {:?} has connected...", clientaddr);
-        let spkt = spkt.serialize_packet();
-        stream.write_all(&spkt[0..spkt.len()])?;
+    pub fn send_packet(&mut self, spkt: &Packet) -> std::io::Result<()> {
+        while self.packets_total < 10 {
+            let addr = SocketAddrV4::new(Ipv4Addr::from_str(&spkt.src[0..]).unwrap(), 8080);
+            let listener = TcpListener::bind(addr)?;
+            let (mut stream, clientaddr) = listener.accept().expect("Unable to accept client");
+            println!("Client: {:?} has connected...", clientaddr);
+            let spkt = spkt.serialize_packet();
+            stream.write_all(&spkt[0..spkt.len()])?;
+            self.packets_sent += 1;
+            self.packets_total += 1;
+        }
         Ok(())
     }
 }
